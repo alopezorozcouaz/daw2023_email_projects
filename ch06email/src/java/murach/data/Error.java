78ff0b4c3084b0763e22fbc70969e63276fbdcc1
alopezorozcouaz <147572356+alopezorozcouaz@users.noreply.github.com>
2023-11-09 10:03:47 -0600

    ch06email se agregó la funcionalidad de modificar un usuario

---
+++ ch06email/src/java/murach/data/Error.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package murach.data;
+
+/**
+ *
+ * @author alopezorozco
+ */
+public class Error {
+    public static String descripcion;   // almacena el mensaje de error, si este ocurre
+}

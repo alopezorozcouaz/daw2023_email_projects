d0e8fb15d10be29cb72ef62e20962c57d01d96da
alopezorozcouaz <147572356+alopezorozcouaz@users.noreply.github.com>
2023-11-08 21:13:53 -0600

    1. Se agregó la librería JSTL, 2. Se escribió el código en la página index.HTML    para mostrar un botón para visualizar el listado de usuarios. 4. Se implemento el código en el servlet para redireccionar a la página    listado-de-usuarios cuando se haga este request. 4. Se creo la página listado-de-usuarios. (Falta agregar el funcionamiento    de los botones Modificar y eliminar).

--- ch04email/src/java/murach/email/EmailListServlet.java
+++ ch04email/src/java/murach/email/EmailListServlet.java
@@ -5,8 +5,8 @@ package murach.email;
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 import java.io.IOException;
+import java.util.List;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -31,52 +31,64 @@ public class EmailListServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        
+
         String url = "/index.html";
-        
+
         //get current action
         String action = request.getParameter("action");
-        if (action == null){
+        if (action == null) {
             action = "join"; //default action
         }
-        
+
         // perform action and set URL to appropiate page
-        if (action.equals("join")){
+        if (action.equals("join")) {
             url = "/index.html";
-        }else if (action.equals("add")){
+        }else if (action.equals("listado")){
+            //declaramos un objeto de tipo ArrayList
+            List<User> users = UserDB.getAllUsers();
+            
+            //establecemos el valor del atributo users con la lista obtenida d
+            //usuarios
+            request.setAttribute("users", users);         
+            
+            // especificamos la página a mostrar
+            url = "/listado-de-usuarios.jsp";
+        }else if (action.equals("add")) {
             //getParameters from the request
             request.setCharacterEncoding("UTF-8");
+            response.setCharacterEncoding("UTF-8");
             String firstName = request.getParameter("firstName");
             String lastName = request.getParameter("lastName");
             String email = request.getParameter("email");
-            
+
+            System.out.println(lastName);
             // store data in User object and save User Object in database
             User user = new User(firstName, lastName, email);
             int result = UserDB.insert(user);
-            
+
             //si resultado es mayor que 0 entonces se realizó con éxito la operación
-            if (result>0){
+            if (result > 0) {
                 request.setAttribute("mensaje", "El usuario fue dado de alta en la bd");
                 request.setAttribute("user", user);
                 url = "/thanks.jsp"; // the "thanks" page
-            }else{
+            } else {
                 request.setAttribute("mensaje", "Hubo un problema al querer guardar la información en la base de datos");
                 request.setAttribute("error", murach.data.Error.descripcion);
                 url = "/error.jsp";
             }
         }
-        
+
         //forward request and response objects to specified URL
         getServletContext()
                 .getRequestDispatcher(url)
                 .forward(request, response);
     }
-    
+
     @Override
-    protected void doGet(HttpServletRequest request, 
-                          HttpServletResponse response) 
-                          throws ServletException, IOException {
+    protected void doGet(HttpServletRequest request,
+            HttpServletResponse response)
+            throws ServletException, IOException {
         doPost(request, response);
-    }   
+    }
 
 }
